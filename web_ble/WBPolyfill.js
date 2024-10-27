3a5c6fe8e9355352bd9bdb036e24166da7d3d39b
Fritz Schmid <fritz@schmid.ai>
2024-10-27 21:28:32 +0100

    🐛 fix(WBPolyfill.js): correct method name in bluetooth.connectDevice function The method name in the bluetooth.connectDevice function has been corrected from "requestDevice" to "connectDevice" to accurately reflect the intended functionality.

--- web_ble/WBPolyfill.js
+++ web_ble/WBPolyfill.js
@@ -119,7 +119,7 @@
 
   bluetooth.connectDevice = async function (deviceId) {
     return native
-      .sendMessage("requestDevice", { data: { deviceId: deviceId } })
+      .sendMessage("connectDevice", { data: { deviceId: deviceId } })
       .then(function (device) {
         return new wb.BluetoothDevice(device);
       });
